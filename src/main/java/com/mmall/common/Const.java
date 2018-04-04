5d4eda81bd8d16e3f876fc2a8fc9f14abaa01056
heylinlook <1124281072@qq.com>
2018-04-04 22:49:16 +0800

    add user service module:login,logout,register,check,forget pwd and local cache

---
+++ src/main/java/com/mmall/common/Const.java
@@ -0,0 +1,29 @@
+package com.mmall.common;
+
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+    public static final String USER_NO_EXIST = "用户名不存在";
+    public static final String PASSWORD_ERROR = "密码错误";
+    public static final String LOGIN_SUCCESS = "登录成功";
+    public static final String USER_NAME_EXIST = "用户名已存在";
+    public static final String EMAIL_EXIST = "邮箱已存在";
+    public static final String LOGOUT_SUCCESS = "退出登录";
+    public static final String REGISTER_ERR = "注册失败";
+    public static final String REGISTER_SUCCESS = "注册成功";
+    public static final String CHECK_SUCCESS = "校验成功";
+    public static final String CHECK_FAILED = "校验失败";
+    public static final String USER_NO_LOGIN = "用户未登录，无法获取用户信息";
+    public static final String QUESTION_EMPTY = "找回问题的密码是空的";
+    public static final String ANSWER_WRONG = "问题的答案错误";
+
+    public static final String USERNAME_TYPE = "username";
+    public static final String EMAIL_TYPE = "email";
+
+    public static final String TOKEN_KEY_PREFIX = "token_";
+
+    public interface Role{
+        int ROLE_CUSTOMER = 0; //普通用户
+        int ROLE_ADMIN = 1;    //管理员
+    }
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,112 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.common.TokenCache;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service("iUserService")
+public class UserServiceImpl implements IUserService {
+
+    @Autowired
+    UserMapper mUserMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int result = mUserMapper.checkUsername(username);
+        if (result == 0) {
+            return ServerResponse.createByErrorMsg(Const.USER_NO_EXIST);
+        }
+        //MD5密码登录
+        String MD5Password = MD5Util.MD5EncodeUtf8(password);
+
+        User user = mUserMapper.selectLogin(username, MD5Password);
+        if(user == null) {
+            return ServerResponse.createByErrorMsg(Const.PASSWORD_ERROR);
+        }
+        user.setPassword(StringUtils.EMPTY);
+        return ServerResponse.createBySuccess(Const.LOGIN_SUCCESS, user);
+    }
+
+    public ServerResponse<String> register(User user) {
+        //校验用户名是否存在
+        ServerResponse valid = this.checkValid(user.getUsername(), Const.USERNAME_TYPE);
+        if (!valid.isSuccessful()) {
+            return valid;
+        }
+        //校验邮箱是否存在
+        valid = this.checkValid(user.getEmail(), Const.EMAIL_TYPE);
+        if (!valid.isSuccessful()) {
+            return valid;
+        }
+
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+
+        int result = mUserMapper.insert(user);
+        if(result == 0) {
+            return ServerResponse.createByErrorMsg(Const.REGISTER_ERR);
+        }
+        return ServerResponse.createBySuccessMessage(Const.REGISTER_SUCCESS);
+    }
+
+    @Override
+    public ServerResponse<String> checkValid(String str, String type) {
+        if(StringUtils.isNotBlank(str)) {
+            if(Const.USERNAME_TYPE.equals(type)) {
+                //校验用户名是否存在
+                int result = mUserMapper.checkUsername(str);
+                if (result > 0) {
+                    return ServerResponse.createByErrorMsg(Const.USER_NAME_EXIST);
+                }
+            } else if(Const.EMAIL_TYPE.equals(type)){
+                //校验邮箱是否存在
+                int result = mUserMapper.checkEmail(str);
+                if (result > 0) {
+                    return ServerResponse.createByErrorMsg(Const.EMAIL_EXIST);
+                }
+            }
+        } else {
+            return ServerResponse.createByErrorMsg(Const.CHECK_FAILED);
+        }
+        return ServerResponse.createBySuccessMessage(Const.CHECK_SUCCESS);
+    }
+
+    @Override
+    public ServerResponse<String> selectQuestion(String username) {
+        ServerResponse valid = this.checkValid(username, Const.USERNAME_TYPE);
+        if(valid.isSuccessful()) {
+            //用户名不存在
+            return ServerResponse.createByErrorMsg(Const.USER_NO_EXIST);
+        }
+        String question = mUserMapper.selectQuestionByUsername(username);
+        if(StringUtils.isNotBlank(question)) {
+            return ServerResponse.createBySuccessMessage(question);
+        }
+        return ServerResponse.createByErrorMsg(Const.QUESTION_EMPTY);
+    }
+
+    @Override
+    public ServerResponse<String> checkAnswer(String username, String question, String answer) {
+        int result = mUserMapper.checkAnswer(username, question, answer);
+        if (result > 0) {
+            //说明用户答对了
+            String forgotToken = UUID.randomUUID().toString();
+            TokenCache.setKey(Const.TOKEN_KEY_PREFIX + username, forgotToken);
+            return ServerResponse.createBySuccessMessage(forgotToken);
+        }
+        //用户没答对
+        return ServerResponse.createByErrorMsg(Const.ANSWER_WRONG);
+    }
+
+
+}
